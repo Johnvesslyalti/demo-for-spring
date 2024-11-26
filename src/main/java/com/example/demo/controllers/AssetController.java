d7da397cd7b3ce741334fa3a81e30c5053aca225
johnvesslyalti <altijohnvessly@gmail.com>
2024-11-26 15:48:21 +0530

    here i commited with how to get with an id with indivisual products

--- src/main/java/com/example/demo/controllers/AssetController.java
+++ src/main/java/com/example/demo/controllers/AssetController.java
@@ -1,27 +1,47 @@
 package com.example.demo.controllers;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.Assets;
-import com.example.demo.services.*;
-
+import com.example.demo.services.AssetService;
 
 @RequestMapping("/assets/")
 @RestController
 @CrossOrigin("*")
 public class AssetController {
 
-	@Autowired
-	public AssetService assetServiceData;
-	
-	@GetMapping("getAll")
-	public ArrayList<Assets> assetServices(){
-		return assetServiceData.getAssetServiceData();
-	}
+    @Autowired
+    private AssetService assetService;
+
+    @GetMapping("getAll")
+    public List<Assets> assetServices() {
+        return assetService.getAssets();
+    }
+
+    @PostMapping("addAssets")
+    public int addAsset(@RequestBody Assets assets) {
+        // You might want to implement the logic to add the asset to a database or in-memory store
+        System.out.println("Adding Asset: " + assets.getAssetName());
+        return 10; // Simulated response
+    }
+
+    @GetMapping(value = "assets/{assetId}")
+    public Assets getAsset(@PathVariable int assetId) {
+        List<Assets> allAssets = assetService.getAssets();
+        for (Assets asset : allAssets) {
+            if (asset.getAssetId() == assetId) {
+                return asset;
+            }
+        }
+        return null;  // Or throw a custom exception if not found
+    }
 }
--- src/main/java/com/example/demo/services/AssetService.java
+++ src/main/java/com/example/demo/services/AssetService.java
@@ -1,5 +1,6 @@
 package com.example.demo.services;
 
+import java.util.List;
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Service;
@@ -7,9 +8,13 @@ import com.example.demo.entities.Assets;
 
 @Service
 public class AssetService {
-	public ArrayList<Assets> getAssetServiceData(){
-		ArrayList<Assets> assets = new ArrayList<Assets>();
-		assets.add(new Assets(1, "Laptop", "100,000"));
-		return assets;
-	}
+
+    public List<Assets> getAssets() {
+        List<Assets> assets = new ArrayList<>();
+        assets.add(new Assets(1, "Laptop", 100000.0));
+        assets.add(new Assets(2, "Projector", 150000.0));
+        assets.add(new Assets(3, "Chairs", 15000.0));
+        assets.add(new Assets(4, "Tables", 10000.0));
+        return assets;
+    }
 }
